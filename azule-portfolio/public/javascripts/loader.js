53234e39464b324b8c4098c35150d2f798dce5aa
AZULENE_NOTE\azule <hibiki20010105@softbank.ne.jp>
2023-04-17 17:48:24 +0900

    loaderの実装

--- azule-portfolio/public/javascripts/loader.js
+++ azule-portfolio/public/javascripts/loader.js
@@ -2,7 +2,7 @@ var loadFinished = false
 var showFinished = false
 
 function fadeOut() {
-    if(loadFinished && showFinished && false) {
+    if(loadFinished && showFinished) {
         anime({
             targets: '#loader',
             opacity: {
@@ -24,16 +24,30 @@ $(function(){
         $(this).html(newHtml);
     });
 
-    var showWords = $('#loader .loader-show-word');
-    showWords.each(function(i){
-        $(this).delay(50 * i).queue(function(){
-            $(this).removeClass('loader-hidden').dequeue();
-        });
+    $('#loader > .show-first .loader-show-word').each(function(i) {
+        $(this).removeClass("loader-first-word");
+        $(this).addClass("loader-first-word");
     });
+
+    var firstWords = $('#loader .loader-first-word')
+    firstWords.each(function(i){
+       $(this).delay(80 * i).queue(function(){
+           $(this).removeClass('loader-hidden').dequeue();
+       });
+    });
+
     setTimeout(function(){
-        showFinished = true;
-        fadeOut();
-    }, 50 * (showWords.length + 5));
+        var showWords = $('#loader .loader-show-word');
+        showWords.each(function(i){
+            $(this).delay(3 * i).queue(function(){
+                $(this).removeClass('loader-hidden').dequeue();
+            });
+        });
+        setTimeout(function(){
+            showFinished = true;
+            fadeOut();
+        }, showWords.length * 3 - 120);
+    }, firstWords.length * 80 + 400);
 });
 
 $(window).on('load', function() {
